65665d451daae6c2d32b0da908eb9b2cad100373
Vishalpcd <vpathak154@gmail.com>
2017-07-25 10:17:35 +0530

    added the coding for the  CaesarCipher and successfully running.

--- src/Unit1/CaesarCipher.java
+++ src/Unit1/CaesarCipher.java
@@ -1,5 +1,29 @@
 package Unit1;
 
+import java.util.Scanner;
+
 public class CaesarCipher {
+	public static void main(String args[])
+	{
+		char c,a;
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter Plain text ");
+		String PT=sc.next();
+		PT=PT.replace(" ", "");
+		PT=PT.toLowerCase();
+		for(int i=0;i<PT.length();i++)
+		{
+			c=PT.charAt(i);
+			if((int)c+3>122)
+			{
+				a=(char)(c+3-26);
+			}
+			else
+			{
+				a=(char)(c+3);
+			}
+			System.out.print(a);
+		}
+	}
 
 }
